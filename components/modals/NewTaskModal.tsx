f4916b7f321f29b9236e8d62d16510e1764631a1
ar-sudchat <artitpj3@gmail.com>
2026-01-20 09:03:55 +0700

    Expand Assignee field width

--- components/modals/NewTaskModal.tsx
+++ components/modals/NewTaskModal.tsx
@@ -570,7 +570,7 @@ export function NewTaskModal({
                                 </div>
 
                                 {/* Row 3: Assignee & Created By */}
-                                <div className="grid grid-cols-2 gap-4">
+                                <div className="space-y-4">
                                     <div>
                                         <label className="block text-sm font-medium text-slate-700 mb-1 flex justify-between">
                                             Assignee
